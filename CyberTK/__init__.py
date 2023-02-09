22a9c465dfb960d4f7e4de09e581aaac26f56c18
cetuch <53572644+cetuch@users.noreply.github.com>
2023-02-09 16:45:09 +0700

    Add files via upload

---
+++ CyberTK/__init__.py
@@ -0,0 +1,6 @@
+__copyright__       = 'Copyright 2021-2022 CyberTK'
+__version__         = '1.0 - beta'
+__license__         = 'BSD 3-Clause'
+__author__          = 'CybeTK'
+__url__             = 'http://github.com/CyberTKR'
+
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", 'r',encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name='LineBot',
+    version='0.1.0',
+    description='Simple-LINELIB',
+    long_description=long_description,
+    author='Tolg KR',
+    author_email='tolg@cybertkr.com',
+    url='https://github.com/CyberTKR/Simple-LINELIB',
+    packages=find_packages(include=['CyberTK', 'CyberTK.*']),
+    install_requires=[
+        'httpx==0.19.0',
+        'requests',
+        'thrift',
+        'CyberTKAPI'
+    ],
+    extras_require={'httpx': ['http2']}
+)
